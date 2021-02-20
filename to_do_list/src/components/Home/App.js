756f4cd490d9bf69075710a2956fc03441520836
andryskowski <52753339+andryskowski@users.noreply.github.com>
2021-02-20 21:47:05 +0100

    bmr is displayed on Home page

--- to_do_list/src/components/Home/App.js
+++ to_do_list/src/components/Home/App.js
@@ -3,18 +3,24 @@ import './sass/App.sass';
 import { useSelector } from 'react-redux';
 
 const App = () => {
-    // const BMRInformations = useSelector(store => store.BMRInformations);
-    // // BMRInformations.filter(info => info.height);
-    // const bmrbmr = JSON.stringify(BMRInformations);
+    const BMRInformations = useSelector(store => store.BMRInformations);
+
+    const INFO_BMR_ELEMENTS = BMRInformations.map(info => (
+        info.BMR
+    ));
+
+    const BMR_INFO = JSON.stringify(INFO_BMR_ELEMENTS);
+
+    console.log(BMR_INFO);
 
     return (
-        
+
         <div className="content">
             <div className="elementsFlex">
-            {/* <div>{bmrbmr}</div> */}
+                <div>{BMR_INFO}</div>
             </div>
         </div>
-        
+
     );
 };
 
