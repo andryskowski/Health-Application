b11a01aee0bfd33144fe0b60e6cb96f477aa7278
andryskowski <52753339+andryskowski@users.noreply.github.com>
2021-02-22 17:47:30 +0100

    added async method 'weatherData' to get informations about weather from api

--- to_do_list/src/components/Home/WeatherWidget.js
+++ to_do_list/src/components/Home/WeatherWidget.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 const WeatherWidget = () => {
 
-   
+    const API_KEY = `9215f8be73c158f8f88c53de2a692d57`;
+    async function weatherData(){
+        
+        await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=${API_KEY}`
+        ).then((res) => console.log(res.json() ));
+    }
+
     return (
         <div className="content">
             WeatherWidget
+            <button onClick={weatherData()}>WeatherButton</button>
         </div>
 
     );
