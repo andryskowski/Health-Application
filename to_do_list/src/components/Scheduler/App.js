bb608b725a8773977a4208112eef581de9119ab7
andryskowski <52753339+andryskowski@users.noreply.github.com>
2021-03-08 21:18:26 +0100

    added view of month (scheduler component)

--- to_do_list/src/components/Scheduler/App.js
+++ to_do_list/src/components/Scheduler/App.js
@@ -4,26 +4,27 @@ import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
   DayView,
+  MonthView,
   Appointments,
 } from '@devexpress/dx-react-scheduler-material-ui';
+import './sass/App.sass';
 
 const currentDate = '2018-11-01';
 const schedulerData = [
-  { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
+  { startDate: '2018-11-01T09:45', endDate: '2018-11-01T09:46', title: 'Meeting' },
   { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
 ];
 
 export default () => (
-  <Paper>
+  <Paper className="scheduler">
     <Scheduler
       data={schedulerData}
     >
       <ViewState
         currentDate={currentDate}
       />
-      <DayView
-        startDayHour={9}
-        endDayHour={14}
+      <MonthView
+        
       />
       <Appointments />
     </Scheduler>
