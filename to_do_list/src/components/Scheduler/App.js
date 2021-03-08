7e8557388784cffc4eeaa69966a061a160594043
andryskowski <52753339+andryskowski@users.noreply.github.com>
2021-03-08 21:22:40 +0100

    added actualDate

--- to_do_list/src/components/Scheduler/App.js
+++ to_do_list/src/components/Scheduler/App.js
@@ -9,7 +9,7 @@ import {
 } from '@devexpress/dx-react-scheduler-material-ui';
 import './sass/App.sass';
 
-const currentDate = '2018-11-01';
+const currentDate = new Date().toISOString().slice(0, 10);
 const schedulerData = [
   { startDate: '2018-11-01T09:45', endDate: '2018-11-01T09:46', title: 'Meeting' },
   { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
