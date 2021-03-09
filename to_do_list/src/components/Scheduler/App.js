b12789715bb695eada3257637a29ba4e39492177
andryskowski <52753339+andryskowski@users.noreply.github.com>
2021-03-09 14:29:54 +0100

    refactoring, scheduler component

--- to_do_list/src/components/Scheduler/App.js
+++ to_do_list/src/components/Scheduler/App.js
@@ -3,37 +3,48 @@ import Paper from '@material-ui/core/Paper';
 import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
-  DayView,
-  Toolbar,
   MonthView,
+  Toolbar,
+  DateNavigator,
   Appointments,
+  TodayButton,
 } from '@devexpress/dx-react-scheduler-material-ui';
 import './sass/App.sass';
 
-import { DateNavigator } from '@devexpress/dx-react-scheduler-material-ui';
+// import { appointments } from '../../../demo-data/month-appointments';
 
-const currentDate = new Date().toISOString().slice(0, 10);
-const schedulerData = [
-  { startDate: '2018-11-01T09:45', endDate: '2018-11-01T09:46', title: 'Meeting' },
-  { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
-];
+export default class Demo extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-export default () => (
-    
-  <Paper className="scheduler">
-      
-    <Scheduler
-      data={schedulerData}
-    >
-        
-      <ViewState
-        currentDate={currentDate}
-      />
-      <MonthView></MonthView>
-      <Toolbar />
+    this.state = {
+      data: [
+        { startDate: '2021-03-09T09:45', endDate: '2021-03-09T09:46', title: 'Meeting' },
+        { startDate: '2021-03-09T12:00', endDate: '2021-03-09T13:30', title: 'Go to a gym' },
+      ],
+    };
+  }
+
+
+
+  render() {
+    const { data } = this.state;
+    const currentDate = new Date().toISOString().slice(0, 10);
+    return (
+      <Paper className="scheduler">
+        <Scheduler
+          data={data} 
+        >
+          <ViewState
+            defaultCurrentDate={currentDate}
+          />
+          <MonthView />
+          <Toolbar />
           <DateNavigator />
-      
-      <Appointments />
-    </Scheduler>
-  </Paper>
-);
+          <TodayButton />
+          <Appointments />
+        </Scheduler>
+      </Paper>
+    );
+  }
+}
