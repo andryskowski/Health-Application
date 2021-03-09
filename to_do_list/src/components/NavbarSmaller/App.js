223a689489605e556e95e72a33eedb487f99ade7
andryskowski <52753339+andryskowski@users.noreply.github.com>
2021-03-09 16:21:08 +0100

    change style of smaller navbar

--- to_do_list/src/components/NavbarSmaller/App.js
+++ to_do_list/src/components/NavbarSmaller/App.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const App = () => {
     return (
-            <nav className="navbar-expand navbar-light bg-light  vw-2 d-block d-lg-none">
+            <nav className="navbar-expand navbar-dark bg-dark  vw-2 d-block d-lg-none">
                 <ul className="navbar-nav">
                     <li className="nav-item">
                         <Link to="/Home" className="nav-link active" href="#">Home</Link>
