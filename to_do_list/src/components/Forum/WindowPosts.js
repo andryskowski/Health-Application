051bf33164971869d3f15e20f174952323aecd00
andryskowski <52753339+andryskowski@users.noreply.github.com>
2021-03-11 20:10:24 +0100

    fixed displaying posts

--- to_do_list/src/components/Forum/WindowPosts.js
+++ to_do_list/src/components/Forum/WindowPosts.js
@@ -2,18 +2,17 @@ import React, { useEffect, useState } from 'react';
 // import './sass/WindowPosts.sass';
 
 const WindowPosts = () => {
-    const [posts, setPosts] = useState(0);
+    const [posts, setPosts] = useState([]);
     // const array_posts = [];
 
     async function getForumPost() {
         const response = await fetch("http://localhost:8000/posts")
             .then(response => response.json())
             .then(response => {
-                // response.map(resp => array_posts.push(<a>{resp.title}</a>))
-                // array_posts.push(response);
-                // console.log(array_posts);
-                response.map(resp => setPosts(prevState => prevState + 'title:' + resp.title + 'content:' + resp.content + '  '));
-                // setPosts(response);
+                // response.map(resp => setPosts(prevState => prevState + 'title:' + resp.title + 'content:' + resp.content + '  '));
+                response.map(resp => 
+                    setPosts(prevState => [...prevState, <li>{'title: ' + resp.title + ', '}{'content: ' + resp.content}</li>]));
+
                 console.log(posts);
             })
             .catch(() => {
@@ -28,13 +27,18 @@ const WindowPosts = () => {
         getForumPost()
     }, []);
 
+    function showPostsInState() {
+        console.log(posts);
+        console.log(typeof posts);
+    }
+
 
     return (
 
-        <div style={{"width" : "20%"}}>
+        <div style={{"width" : "30%"}}>
             <h1>Posts</h1>
             <div>{posts}</div>
-            
+            <button onClick={showPostsInState}>show posts in state</button>
 
         </div>
 
