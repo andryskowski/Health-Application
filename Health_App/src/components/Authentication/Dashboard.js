79309067ef22565365889ef0f3e48dffdbe75f9d
andryskowski <52753339+andryskowski@users.noreply.github.com>
2021-03-26 14:22:07 +0100

    fixed problem if user doesn't have profile picture

--- Health_App/src/components/Authentication/Dashboard.js
+++ Health_App/src/components/Authentication/Dashboard.js
@@ -30,7 +30,8 @@ export default function Dashboard() {
          setImageAsUrl(fireBaseUrl);
        })
        .catch(() => {
-        alert('Error retrieving data!');
+        setImageAsUrl(`https://i.pinimg.com/originals/0c/3b/3a/0c3b3adb1a7530892e55ef36d3be6cb8.png`);
+        console.log('Error retrieving data! User doesnt have profile picture');
     });
 
   }
