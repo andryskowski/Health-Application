f44812a3c33fb67a0f57b87a20a0a5a6af3414ab
maniahania <a.strzelecka@onet.pl>
2021-05-12 15:17:14 +0200

    home wyglad

--- Health_App/src/components/Home/WeatherWidget.js
+++ Health_App/src/components/Home/WeatherWidget.js
@@ -1,62 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
-import './sass/App.sass';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import "./sass/App.sass";
 
 const WeatherWidget = () => {
-    //geolocation
-    const [LAT, setLat] = useState(0);
-    const [LON, setLon] = useState(0);
-    const [ACTUAL_TEMPERATURE, setActualTemperature] = useState(0);
-    const [WEATHER_DESCRIPTION, setWeatherDescription] = useState(0);
-    const [WEATHER_LOCATION, setWeatherLocation] = useState(0);
-    const [WEATHER_ICON, setWeatherIcon] = useState(0);
+  //geolocation
+  const [LAT, setLat] = useState(0);
+  const [LON, setLon] = useState(0);
+  const [ACTUAL_TEMPERATURE, setActualTemperature] = useState(0);
+  const [WEATHER_DESCRIPTION, setWeatherDescription] = useState(0);
+  const [WEATHER_LOCATION, setWeatherLocation] = useState(0);
+  const [WEATHER_ICON, setWeatherIcon] = useState(0);
 
-    //get actual user location
-    function getLocation() {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(setPosition);
-        } else {
-          console.log("Geolocation is not supported by this browser.");
-        }
-      }
-
-    function setPosition(position) {
-        setLat(position.coords.latitude);
-        setLon(position.coords.longitude);
-      }
-
-    //connection with API (OpenWeatherMap)
-
-    const API_KEY = `9215f8be73c158f8f88c53de2a692d57`;
-
-    //get info about weather from weather APi
-    async function getWeatherData(){
-        getLocation();
-        const URL = `http://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${API_KEY}`;
-        const apiRES = await fetch(URL).then((res) => res.json());
-        setActualTemperature(Math.ceil(apiRES.main.temp - 273.15));
-        setWeatherDescription(apiRES.weather[0].description);
-        setWeatherLocation(apiRES.name);
-        const iconURL = `http://openweathermap.org/img/wn/${apiRES.weather[0].icon}.png`;
-        setWeatherIcon(iconURL);
+  //get actual user location
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(setPosition);
+    } else {
+      console.log("Geolocation is not supported by this browser.");
     }
+  }
 
-    useEffect(()=>{
-     getWeatherData();
-    });
+  function setPosition(position) {
+    setLat(position.coords.latitude);
+    setLon(position.coords.longitude);
+  }
 
-    return (
-        <>
-            <div className="weather-widget widget">
-            <div class="first-row">
-            <h1 class="weather-temperature">{ACTUAL_TEMPERATURE}&#176;C</h1>
-            <img class="weather-icon" src={WEATHER_ICON}></img>
-            </div>
-            <h3>{WEATHER_LOCATION}, {WEATHER_DESCRIPTION}</h3>
-            </div>
-        </>
+  //connection with API (OpenWeatherMap)
 
-    );
+  const API_KEY = `9215f8be73c158f8f88c53de2a692d57`;
+
+  //get info about weather from weather APi
+  async function getWeatherData() {
+    getLocation();
+    const URL = `http://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${API_KEY}`;
+    const apiRES = await fetch(URL).then((res) => res.json());
+    setActualTemperature(Math.ceil(apiRES.main.temp - 273.15));
+    setWeatherDescription(apiRES.weather[0].description);
+    setWeatherLocation(apiRES.name);
+    const iconURL = `http://openweathermap.org/img/wn/${apiRES.weather[0].icon}.png`;
+    setWeatherIcon(iconURL);
+  }
+
+  useEffect(() => {
+    getWeatherData();
+  });
+
+  return (
+    <>
+      <div className="weather-widget widget first-widget">
+        <div class="first-row">
+          <h1 class="weather-temperature">{ACTUAL_TEMPERATURE}&#176;C</h1>
+          <img class="weather-icon" src={WEATHER_ICON}></img>
+        </div>
+        <h3>
+          {WEATHER_LOCATION}, {WEATHER_DESCRIPTION}
+        </h3>
+      </div>
+    </>
+  );
 };
 
 export default WeatherWidget;
