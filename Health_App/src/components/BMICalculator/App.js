8263724fea51f0e95af4e5c1a2dce7c4a41225a9
karol <karol@DESKTOP-6V4ABNI>
2021-08-17 10:39:22 +0200

    Changed font and layout in BMI calc

--- Health_App/src/components/BMICalculator/App.js
+++ Health_App/src/components/BMICalculator/App.js
@@ -76,15 +76,11 @@ function App() {
             <div>
               <img class="image" src={bikeImg}></img>
             </div>
-            
-            
           </div>
-          
-          <h4 className="mt-2">Your BMI is: {bmi}</h4>
+          <h4 className="mt-1">Your BMI is: {bmi}</h4>
           {bmi != 0 ? (
             <h4>
-              It means <span style={{ color: `${resultColor}` }}>{result}</span>
-              .
+              It means: <span class="itmeans" style={{ color: `${resultColor}` }}>{result}</span>
             </h4>
           ) : (
             <h4> It means ...</h4>
@@ -96,7 +92,7 @@ function App() {
               BMI takes into account natural variations in body shape, giving a
               healthy weight range for a particular height. As well as measuring
               your BMI, healthcare professionals may take other factors into
-              account when assessing if you're a healthy weight.0 Muscle is much
+              account when assessing if you're a healthy weight. Muscle is much
               denser than fat, so very muscular people, such as heavyweight
               boxers, weight trainers and athletes, may be a healthy weight even
               though their BMI is classed as obese. Your ethnic group can also
